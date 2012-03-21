302dc162221ebd5c7f4b50c4ed2021c4edd8bb60
Ksenia Krasheninnikova <krasheninnikova@gmail.com>
2012-03-21 19:22:29 +0300

    functions for creating local configs

--- src/utils/genutils.cpp
+++ src/utils/genutils.cpp
@@ -9,13 +9,17 @@ namespace utils{
 	  return std::ifstream( fname.c_str() ,  std::ifstream::in ) != NULL;
 	}
 
-	
-	void makeConfig(std::string const &fname)
+
+	template < class T >	
+	void makeConfig(std::string const &fname, T const & configer )
 	{
 		if (! fileExists(fname) )
 		{	
 			std::ofstream file;
 			file.open ( fname.c_str() );
+			
+			configer.generate(file);
+
 			file.close ();
 		}
 
