6af9e6aef29fe198f90f86af4d07f2713a855f79
Ksenia Krasheninnikova <krasheninnikova@gmail.com>
2012-04-11 22:02:54 +0400

    one more commit for genutils.cpp

--- src/utils/genutils.cpp
+++ src/utils/genutils.cpp
@@ -9,60 +9,57 @@
 
 namespace utils{
 		
-	using std::string;
+    using std::string;
     using std::runtime_error;
 
-	bool fileExists( string const &fname )
-	{
-	  return std::ifstream( fname.c_str() ,  std::ifstream::in ) != NULL;
-	}
+    bool fileExists( string const &fname )
+    {
+        return std::ifstream( fname.c_str() ,  std::ifstream::in );
+    }
 
-
-	ProxySettings readProxySettings( string const & fname )
-	{
-		return ProxySettings ( fname );
-	}
+    ProxySettings readProxySettings( string const & fname )
+    {
+        return ProxySettings ( fname );
+    }
 
     void readAllProxySettings ( string const & dir, 
                                 map< string, ProxySettings > & allSettings )
-	{
-		files_t allFiles; 
+    {
+        files_t allFiles; 
         filter_dir_files( FileInfo(dir).getFullName() , allFiles ); // usr home dir
-		files_t::const_iterator afit = allFiles.begin();
 
-		for ( ; afit != allFiles.end(); ++afit )
-		{
-			allSettings.insert( std::make_pair ( afit->getName()  , 
+        for ( files_t::const_iterator afit = allFiles.begin() ; afit != allFiles.end(); ++afit )
+        {
+            allSettings.insert( std::make_pair ( afit->getName()  , 
                                                 ProxySettings( afit->getFullName() )) );
-		}
-	}
+        }
+    }
 
-	pair < string, string > getParentAndFile ( string const & fname )
-	{
+    pair < string, string > getParentAndFile ( string const & fname )
+    {
         std::vector < string > split_vec; 
-		boost::split( split_vec, fname, boost::is_any_of("/") );
-		string name = split_vec.back();
+        boost::split( split_vec, fname, boost::is_any_of("/") );
+        string name = split_vec.back();
         string dir = fname;
-		return make_pair ( dir.erase ( fname.find_last_of (name) ), name ) ;
+        return make_pair ( dir.erase ( fname.find_last_of (name) ), name ) ;
 	}
 
-	template < class T >	
-	void makeConfig(string const &fname, T const & configer )
-	{
-		if (! fileExists(fname) )
-		{	
-			std::ofstream file;
-			
-            pair < string, string > p = getParentAndFile( fname );
+    template < class T >	
+    void makeConfig(string const &fname, T const & configer )
+    {
+        if (! fileExists(fname) )
+        {	
+            std::ofstream file;
+			pair < string, string > p = getParentAndFile( fname );
 		   
-			create_dir( p.first );
-			file.open ( p.second.c_str() );
+            create_dir( p.first );
+            file.open ( p.second.c_str() );
 			
-			configer.generate(file);
+            configer.generate(file);
 
-			file.close ();
-		}
+            file.close ();
+        }
 
-	}
-
+    }
+                                                                
 } // namespace utils
